f94777509090cc73f80c36676bc5600c48cdef32
UmarAbdullah79 <uu0339843@gmail.com>
2025-08-23 17:03:43 +0530

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Umar</h1>
+    </>
+  );
+}
+
+export default App;
