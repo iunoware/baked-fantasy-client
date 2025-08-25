020beaf13216e48113b2b616b5688fa65e8610f2
Praveen123-png <praveenkumar814444@gmail.com>
2025-08-25 13:33:57 +0530

    navbar fully complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,68 +1,102 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 function Navbar() {
+  let [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <>
       <header className="bg-[#ffffffa2] backdrop-blur-sm fixed w-full top-0 shadow-lg">
         <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
           <div className="flex h-16 items-center justify-between">
             <div className="md:flex md:items-center md:gap-12">
-              <Link to="/" className="block text-sky-600 text-xl">
+              <Link to="/" className="block text-sky-600 text-lg ">
                 THE BACKED FANTASY
               </Link>
             </div>
 
-            {/* Home */}
             <div className="hidden md:block">
               <nav aria-label="Global">
                 <ul className="flex items-center gap-6 text-sm">
+                  {/* Home */}
                   <li>
-                    <Link
+                    <NavLink
                       to="/"
-                      className="text-gray-500 transition hover:text-black text-lg hover:bg-sky-300 rounded-lg p-3"
+                      className={({ isActive }) =>
+                        `text-lg rounded-lg p-3 transition ${
+                          isActive
+                            ? "bg-sky-500 text-white font-semibold"
+                            : "hover:text-black text-gray-500 hover:bg-sky-200"
+                        } `
+                      }
                     >
                       Home
-                    </Link>
+                    </NavLink>
                   </li>
 
                   {/* about us */}
                   <li>
-                    <Link
+                    <NavLink
                       to="/about"
-                      className="text-gray-500 transition hover:text-black text-lg hover:bg-sky-300 rounded-lg p-3"
+                      className={({ isActive }) =>
+                        `text-lg rounded-lg p-3 transition ${
+                          isActive
+                            ? "bg-sky-500 text-white font-semibold"
+                            : "hover:text-black text-gray-500 hover:bg-sky-200"
+                        } `
+                      }
                     >
                       About
-                    </Link>
+                    </NavLink>
                   </li>
 
                   {/* products */}
                   <li>
-                    <Link
+                    <NavLink
                       to="/products"
-                      className="text-gray-500 transition hover:text-black text-lg hover:bg-sky-300 rounded-lg p-3"
+                      className={({ isActive }) =>
+                        `text-lg rounded-lg p-3 transition ${
+                          isActive
+                            ? "bg-sky-500 text-white font-semibold"
+                            : "hover:text-black text-gray-500 hover:bg-sky-200"
+                        } `
+                      }
                     >
                       Products
-                    </Link>
+                    </NavLink>
                   </li>
 
                   {/* courses */}
                   <li>
-                    <Link
+                    <NavLink
                       to="/courses"
-                      className="text-gray-500 transition hover:text-black text-lg hover:bg-sky-300 rounded-lg p-3"
+                      className={({ isActive }) =>
+                        `text-lg rounded-lg p-3 transition ${
+                          isActive
+                            ? "bg-sky-500 text-white font-semibold"
+                            : "hover:text-black text-gray-500 hover:bg-sky-200"
+                        } `
+                      }
                     >
                       Courses
-                    </Link>
+                    </NavLink>
                   </li>
 
                   {/* contact */}
                   <li>
-                    <Link
+                    <NavLink
                       to="/contact"
-                      className="text-gray-500 transition hover:text-black text-lg hover:bg-sky-300 rounded-lg p-3"
+                      className={({ isActive }) =>
+                        `text-lg rounded-lg p-3 transition ${
+                          isActive
+                            ? "bg-sky-500 text-white font-semibold"
+                            : "hover:text-black text-gray-500 hover:bg-sky-200"
+                        } `
+                      }
                     >
                       Contact
-                    </Link>
+                    </NavLink>
                   </li>
                 </ul>
               </nav>
@@ -88,7 +122,10 @@ function Navbar() {
               </div>
 
               <div className="block md:hidden">
-                <button className="rounded-sm bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
+                <button
+                  onClick={() => setIsMenuOpen(!isMenuOpen)}
+                  className="rounded-sm bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75"
+                >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="size-5"
@@ -109,6 +146,94 @@ function Navbar() {
           </div>
         </div>
       </header>
+
+      {isMenuOpen && (
+        <div className="">
+          <nav className="fixed top-16 left-0 w-full bg-[#ffffffa2] backdrop-blur-sm shadow-md md:hidden transition-all duration-200">
+            <ul className="flex flex-col items-start p-4 space-y-4">
+              {/* home */}
+              <li>
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    `text-lg rounded-lg p-3 w-full transition ${
+                      isActive
+                        ? "bg-sky-500 text-white font-semibold"
+                        : "hover:text-black text-black hover:bg-sky-200"
+                    } `
+                  }
+                >
+                  Home
+                </NavLink>
+              </li>
+
+              {/* about */}
+              <li>
+                <NavLink
+                  to="/about"
+                  className={({ isActive }) =>
+                    `text-lg rounded-lg p-3 transition ${
+                      isActive
+                        ? "bg-sky-500 text-white font-semibold"
+                        : "hover:text-black text-black hover:bg-sky-200"
+                    } `
+                  }
+                >
+                  About
+                </NavLink>
+              </li>
+
+              {/* products */}
+              <li>
+                <NavLink
+                  to="/products"
+                  className={({ isActive }) =>
+                    `text-lg rounded-lg p-3 transition ${
+                      isActive
+                        ? "bg-sky-500 text-white font-semibold"
+                        : "hover:text-black text-black hover:bg-sky-200"
+                    } `
+                  }
+                >
+                  Products
+                </NavLink>
+              </li>
+
+              {/* courses */}
+              <li>
+                <NavLink
+                  to="/courses"
+                  className={({ isActive }) =>
+                    `text-lg rounded-lg p-3 transition ${
+                      isActive
+                        ? "bg-sky-500 text-white font-semibold"
+                        : "hover:text-black text-black hover:bg-sky-200"
+                    } `
+                  }
+                >
+                  Courses
+                </NavLink>
+              </li>
+
+              {/* contact */}
+              <li>
+                <NavLink
+                  to="/contact"
+                  className={({ isActive }) =>
+                    `text-lg rounded-lg p-3 transition ${
+                      isActive
+                        ? "bg-sky-500 text-white font-semibold"
+                        : "hover:text-black text-black hover:bg-sky-200"
+                    } `
+                  }
+                >
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      )}
     </>
   );
 }
