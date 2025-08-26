8472837aa365524e3dab132b1a5e18ccc4da0efe
UmarAbdullah79 <uu0339843@gmail.com>
2025-08-26 19:08:50 +0530

    hero and cards section finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,19 +7,23 @@ function Navbar() {
 
   return (
     <>
-      <header className="w-full top-3 absolute">
+      <header className="w-full absolute">
         <div className="mx-auto max-w-screen-xl p-4 sm:px-6 lg:px-8">
-          <div className="flex h-16 items-center justify-between">
-            <div className="md:flex md:items-center md:gap-12 absolute">
+          <div className="flex h-16 items-center justify-between w-full ">
+            {/* brand name */}
+            <div className="md:flex md:items-center md:gap-12">
               <Link
                 to="/"
-                className="block text-pink-500 text-2xl font-bold brand-name"
+                className="text-pink-500 text-2xl font-bold brand-name"
               >
                 The Backed Fantasy
               </Link>
             </div>
-
-            <div className="hidden md:block p-4 bg-rose-200 rounded-xl px-[8px] fixed left-[34%]">
+            {/* navbar */}
+            <div
+              className="hidden lg:block fixed top-5 left-1/2 transform -translate-x-1/2 
+                bg-rose-200 rounded-xl p-4 px-[8px] shadow-md z-40 w-[90%] sm:w-[80%] md:w-auto"
+            >
               <nav aria-label="Global">
                 <ul className="flex items-center gap-6 text-sm text-black">
                   {/* Home */}
@@ -104,11 +108,11 @@ function Navbar() {
                 </ul>
               </nav>
             </div>
-
-            <div className="flex items-center gap-4 lg:absolute right-[12%]">
-              <div className="sm:flex sm:gap-4">
+            {/* login and Register */}
+            <div className="flex items-center gap-4 m-5">
+              <div className="sm:flex sm:gap-4 hover:scale-105 ">
                 <a
-                  className="rounded-md bg-sky-500 px-5 py-2.5 text-sm font-medium text-white shadow-sm"
+                  className="rounded-md bg-sky-500 px-5 py-2.5 text-sm font-medium text-white shadow-sm hover:bg-sky-700 "
                   href="#"
                 >
                   Login
@@ -116,7 +120,7 @@ function Navbar() {
 
                 <div className="hidden sm:flex">
                   <a
-                    className="rounded-md bg-pink-500 px-5 py-2.5 text-sm font-medium text-white"
+                    className="rounded-md bg-pink-500 px-5 py-2.5 text-sm font-medium text-white hover:bg-pink-700 hover:scale-105"
                     href="#"
                   >
                     Register
@@ -124,7 +128,7 @@ function Navbar() {
                 </div>
               </div>
 
-              <div className="block md:hidden">
+              <div className="block lg:hidden">
                 <button
                   onClick={() => setIsMenuOpen(!isMenuOpen)}
                   className="rounded-sm bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75"
@@ -152,7 +156,7 @@ function Navbar() {
 
       {isMenuOpen && (
         <div className="">
-          <nav className="fixed top-16 left-0 w-full bg-[#ffffffa2] backdrop-blur-sm shadow-md md:hidden transition-all duration-200">
+          <nav className="fixed top-16 left-0 w-full bg-[#ffffffa2] backdrop-blur-sm shadow-md lg:hidden transition-all duration-200">
             <ul className="flex flex-col items-start p-4 space-y-4">
               {/* home */}
               <li>
