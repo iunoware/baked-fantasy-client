d2fa32e57773b205eb286a393a1131047abd8368
Praveen123-png <praveenkumar814444@gmail.com>
2025-09-02 18:57:15 +0530

    course pages complete

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -2,11 +2,11 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 function ScrollToTop() {
-  const { pathName } = useLocation();
+  const { pathname } = useLocation();
 
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, [pathName]);
+  }, [pathname]);
 
   return null;
 }
--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,76 +1,34 @@
+import CourseCard from "@/components/CourseCard";
+
 function Courses() {
   return (
     <>
-      <h1 className="text-7xl flex justify-center p-30">Courses</h1>
+      {/* hero section */}
+      <div>
+        <h1 className="lg:text-6xl md:text-5xl sm:text-4xl text-3xl flex font-semibold justify-center pt-30">
+          Choose our <span className="text-orange-900">&nbsp;Learning path</span>
+        </h1>
+        <div className="flex justify-center">
+          <p className="text-center lg:text-2xl md:text-xl sm:text-lg max-w-[600px] mx-3 text-gray-700 py-10">
+            Master the art of baking with our expert-led courses designed for all skill
+            levels. Whether you prefer learning online or in-person, we have the perfect
+            program for you.
+          </p>
+        </div>
+      </div>
 
-      <p className="text-2xl mt-20">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit pariatur minus
-        velit dolore. Pariatur doloremque id ex saepe nulla. Ullam esse, nesciunt mollitia
-        odit fugit inventore accusantium quaerat, excepturi maxime, accusamus velit nobis.
-        Aut molestias inventore laudantium repudiandae atque tempora rerum praesentium
-        excepturi, quae est, consectetur fugiat necessitatibus laborum ullam vitae
-        blanditiis asperiores modi dicta doloribus sit, eligendi cupiditate. Ullam earum
-        ab maxime laudantium alias vel reiciendis mollitia quas. Sequi suscipit beatae
-        dicta reprehenderit impedit, facilis doloribus vel harum architecto quam similique
-        esse temporibus rem nostrum. Earum voluptatum consequuntur id et, ullam aut?
-        Perferendis animi odio odit. Eum sapiente ducimus tempora magnam ad harum incidunt
-        aspernatur, eos earum ea numquam, aliquam voluptatem officia! Reiciendis
-        temporibus saepe, rem minus velit eligendi? Architecto dolorum quasi dicta eum
-        inventore, sunt aut quisquam officiis consequuntur, voluptatum eveniet illo
-        molestias at obcaecati! Enim hic consectetur labore mollitia! Reiciendis nostrum
-        distinctio porro doloremque quaerat dolorem enim. Corrupti rem facilis possimus
-        neque vitae maiores esse voluptatem assumenda ad beatae? Consequuntur minus
-        delectus sequi hic fugiat quos nisi et ut. Animi laudantium odit sit iusto,
-        molestiae officia cumque provident aspernatur praesentium numquam rerum ratione.
-        Error vel odio atque ad accusantium, delectus porro nihil illo minima ipsum non
-        doloremque?
-      </p>
+      {/* courses online and offline */}
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-8">
+        {/* online course */}
+        <div className="m-4 lg:ml-20">
+          <CourseCard />
+        </div>
 
-      <p className="text-2xl mt-20">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit pariatur minus
-        velit dolore. Pariatur doloremque id ex saepe nulla. Ullam esse, nesciunt mollitia
-        odit fugit inventore accusantium quaerat, excepturi maxime, accusamus velit nobis.
-        Aut molestias inventore laudantium repudiandae atque tempora rerum praesentium
-        excepturi, quae est, consectetur fugiat necessitatibus laborum ullam vitae
-        blanditiis asperiores modi dicta doloribus sit, eligendi cupiditate. Ullam earum
-        ab maxime laudantium alias vel reiciendis mollitia quas. Sequi suscipit beatae
-        dicta reprehenderit impedit, facilis doloribus vel harum architecto quam similique
-        esse temporibus rem nostrum. Earum voluptatum consequuntur id et, ullam aut?
-        Perferendis animi odio odit. Eum sapiente ducimus tempora magnam ad harum incidunt
-        aspernatur, eos earum ea numquam, aliquam voluptatem officia! Reiciendis
-        temporibus saepe, rem minus velit eligendi? Architecto dolorum quasi dicta eum
-        inventore, sunt aut quisquam officiis consequuntur, voluptatum eveniet illo
-        molestias at obcaecati! Enim hic consectetur labore mollitia! Reiciendis nostrum
-        distinctio porro doloremque quaerat dolorem enim. Corrupti rem facilis possimus
-        neque vitae maiores esse voluptatem assumenda ad beatae? Consequuntur minus
-        delectus sequi hic fugiat quos nisi et ut. Animi laudantium odit sit iusto,
-        molestiae officia cumque provident aspernatur praesentium numquam rerum ratione.
-        Error vel odio atque ad accusantium, delectus porro nihil illo minima ipsum non
-        doloremque?
-      </p>
-
-      <p className="text-2xl mt-20">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit pariatur minus
-        velit dolore. Pariatur doloremque id ex saepe nulla. Ullam esse, nesciunt mollitia
-        odit fugit inventore accusantium quaerat, excepturi maxime, accusamus velit nobis.
-        Aut molestias inventore laudantium repudiandae atque tempora rerum praesentium
-        excepturi, quae est, consectetur fugiat necessitatibus laborum ullam vitae
-        blanditiis asperiores modi dicta doloribus sit, eligendi cupiditate. Ullam earum
-        ab maxime laudantium alias vel reiciendis mollitia quas. Sequi suscipit beatae
-        dicta reprehenderit impedit, facilis doloribus vel harum architecto quam similique
-        esse temporibus rem nostrum. Earum voluptatum consequuntur id et, ullam aut?
-        Perferendis animi odio odit. Eum sapiente ducimus tempora magnam ad harum incidunt
-        aspernatur, eos earum ea numquam, aliquam voluptatem officia! Reiciendis
-        temporibus saepe, rem minus velit eligendi? Architecto dolorum quasi dicta eum
-        inventore, sunt aut quisquam officiis consequuntur, voluptatum eveniet illo
-        molestias at obcaecati! Enim hic consectetur labore mollitia! Reiciendis nostrum
-        distinctio porro doloremque quaerat dolorem enim. Corrupti rem facilis possimus
-        neque vitae maiores esse voluptatem assumenda ad beatae? Consequuntur minus
-        delectus sequi hic fugiat quos nisi et ut. Animi laudantium odit sit iusto,
-        molestiae officia cumque provident aspernatur praesentium numquam rerum ratione.
-        Error vel odio atque ad accusantium, delectus porro nihil illo minima ipsum non
-        doloremque?
-      </p>
+        {/* offline course */}
+        <div className="m-4 lg:mr-20">
+          <CourseCard />
+        </div>
+      </div>
     </>
   );
 }
