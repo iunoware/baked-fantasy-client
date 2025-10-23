28dc5309dc7a0d2bef8523c469fcb189317d1e32
Praveen123-png <praveenkumar814444@gmail.com>
2025-10-23 14:19:04 +0530

    side bar finished

---
+++ src/pages/adminPanel/OrdersAdmin.jsx
@@ -0,0 +1,9 @@
+function OrdersAdmin() {
+  return (
+    <div className="bg">
+      <h1 className="pt-50 pl-50 text-3xl">Orders admin</h1>
+    </div>
+  );
+}
+
+export default OrdersAdmin;
---
+++ src/pages/adminPanel/ProductsAdmin.jsx
@@ -0,0 +1,9 @@
+function ProductAdmin() {
+  return (
+    <div className="bg">
+      <h1 className="pt-50 pl-50 text-3xl">Product admin</h1>
+    </div>
+  );
+}
+
+export default ProductAdmin;
